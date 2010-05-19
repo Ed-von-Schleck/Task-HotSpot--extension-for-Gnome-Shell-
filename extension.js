4721c13600f3fb3703095b608c8b0fdd084c962f
Christian Schramm <christian@localhost.localdomain>
2010-05-19 12:46:11 +0200

    move app switcher to the clock if activated by hotspot and enable hovering from the start

--- extension.js
+++ extension.js
@@ -6,6 +6,8 @@
 
 const Shell = imports.gi.Shell;
 const Gdk = imports.gi.Gdk;
+const Clutter = imports.gi.Clutter;
+const St = imports.gi.St;
 
 const Lang = imports.lang;
 
@@ -103,3 +105,95 @@ AltTab.AltTabPopup.prototype._clickedOutside = function(actor, event) {
     if (ex < x + sx && ex > x && ey < sy)
         _show_next = false;
 }
+
+AltTab.AltTabPopup.prototype._allocate = function (actor, box, flags) {
+    let childBox = new Clutter.ActorBox();
+    let focus = global.get_focus_monitor();
+
+    let leftPadding = this.actor.get_theme_node().get_padding(St.Side.LEFT);
+    let rightPadding = this.actor.get_theme_node().get_padding(St.Side.RIGHT);
+    let bottomPadding = this.actor.get_theme_node().get_padding(St.Side.BOTTOM);
+    let vPadding = this.actor.get_theme_node().get_vertical_padding();
+    let hPadding = leftPadding + rightPadding;
+
+    // Allocate the appSwitcher
+    // We select a size based on an icon size that does not overflow the screen
+    let [childMinHeight, childNaturalHeight] = this._appSwitcher.actor.get_preferred_height(focus.width - hPadding);
+    let [childMinWidth, childNaturalWidth] = this._appSwitcher.actor.get_preferred_width(childNaturalHeight);
+    childBox.x1 = Math.max(focus.x + leftPadding, focus.x + Math.floor((focus.width - childNaturalWidth) / 2));
+    childBox.x2 = Math.min(childBox.x1 + focus.width - hPadding, childBox.x1 + childNaturalWidth);
+    
+    // this is patched to make the app switcher closer to the clock
+    let [ex, ey, mods] = global.get_pointer();
+    if (!(mods & Gdk.ModifierType.MOD1_MASK)) {
+        // alt not pressed:
+        [sx, sy] = Main.panel._clockButton.get_size();
+        childBox.y1 = focus.y + sy;
+    } else {
+        childBox.y1 = focus.y + Math.floor((focus.height - childNaturalHeight) / 2);
+    }
+    childBox.y2 = childBox.y1 + childNaturalHeight;
+    this._appSwitcher.actor.allocate(childBox, flags);
+
+    // Allocate the thumbnails
+    // We try to avoid overflowing the screen so we base the resulting size on
+    // those calculations
+    if (this._thumbnails) {
+        let icon = this._appIcons[this._currentApp].actor;
+        // Force a stage relayout to make sure we get the correct position
+        global.stage.get_actor_at_pos(Clutter.PickMode.REACTIVE, 0, 0);
+        let [posX, posY] = icon.get_transformed_position();
+        let thumbnailCenter = posX + icon.width / 2;
+        let [childMinWidth, childNaturalWidth] = this._thumbnails.actor.get_preferred_width(-1);
+        childBox.x1 = Math.max(focus.x + leftPadding, Math.floor(thumbnailCenter - childNaturalWidth / 2));
+        if (childBox.x1 + childNaturalWidth > focus.x + focus.width - hPadding) {
+            let offset = childBox.x1 + childNaturalWidth - focus.width + hPadding;
+            childBox.x1 = Math.max(focus.x + leftPadding, childBox.x1 - offset - hPadding);
+        }
+
+        let [found, spacing] = this.actor.get_theme_node().get_length('spacing', false);
+        if (!found)
+            spacing = 0;
+
+        childBox.x2 = childBox.x1 +  childNaturalWidth;
+        if (childBox.x2 > focus.x + focus.width - rightPadding)
+            childBox.x2 = focus.x + focus.width - rightPadding;
+        childBox.y1 = this._appSwitcher.actor.allocation.y2 + spacing;
+        this._thumbnails.addClones(focus.height - bottomPadding - childBox.y1);
+        let [childMinHeight, childNaturalHeight] = this._thumbnails.actor.get_preferred_height(-1);
+        childBox.y2 = childBox.y1 + childNaturalHeight;
+        this._thumbnails.actor.allocate(childBox, flags);
+    }
+}
+
+AltTab.AltTabPopup.prototype._init = function() {
+    this.actor = new Shell.GenericContainer({ name: 'altTabPopup',
+                                                reactive: true });
+
+    this.actor.connect('get-preferred-width', Lang.bind(this, this._getPreferredWidth));
+    this.actor.connect('get-preferred-height', Lang.bind(this, this._getPreferredHeight));
+    this.actor.connect('allocate', Lang.bind(this, this._allocate));
+
+    this.actor.connect('destroy', Lang.bind(this, this._onDestroy));
+
+    this._haveModal = false;
+
+    this._currentApp = 0;
+    this._currentWindow = -1;
+    this._thumbnailTimeoutId = 0;
+    this._motionTimeoutId = 0;
+
+    // Initially disable hover so we ignore the enter-event if
+    // the switcher appears underneath the current pointer location
+    // patched to ignore that
+    let [ex, ey, mods] = global.get_pointer();
+    if (mods & Gdk.ModifierType.MOD1_MASK) {
+        // alt pressed:
+        this._disableHover();
+    } else {
+        this._mouseActive = true;
+    }
+
+    Main.uiGroup.add_actor(this.actor);
+}
+
